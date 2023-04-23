be3a1cd24f78296d6847e0b66e5cb74ce3921ec4
nirmaha <86540335+nirmaha@users.noreply.github.com>
2023-04-23 01:01:41 -0500

    Update ArrayList.h

--- ArrayList.h
+++ ArrayList.h
@@ -1,1 +1,10 @@
+typedef struct {
+  int length;
+  char * type;
+} ArrayList;
 
+void add(ArrayList& x, void * elem);
+ArrayList* arrlistcotr();
+
+#define ArrayList.add(x, y) add(x, y)
+#define ArrayList(char * typing)
